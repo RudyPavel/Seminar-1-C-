099eaa92b4801d36d3a4cc03b4cbcd4bc8b7f338
RudyPavel <rudyy.pavlik@mail.ru>
2023-02-15 23:25:47 +0200

    damashnie zadanie

---
+++ zadania 6/Program.cs
@@ -0,0 +1,6 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+    Console.WriteLine ("Yes");
+else
+    Console.WriteLine ("No");
---
+++ zadanie 8/Program.cs
@@ -0,0 +1,12 @@
+﻿int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+   if (i % 2 == 0)
+    {
+        Console.Write(i);
+        Console.Write(" ");
+    }
+}
+
+Console.WriteLine();
